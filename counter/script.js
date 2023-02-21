09719d628707944236849683299c5ea9eecc4a51
Katharina Ephan <ephankatharina@gmail.com>
2023-02-21 13:25:11 +0100

    solves counter kata

---
+++ counter/script.js
@@ -0,0 +1,32 @@
+const resetButton = document.querySelector("button");
+const root = document.documentElement;
+const main = document.querySelector("main");
+
+let counter = 0;
+
+main.addEventListener("click", CounterEvent);
+document.addEventListener("keydown", CounterEvent);
+
+function CounterEvent(event) {
+  if (event.type === "click" || event.key === " " || event.key === "Enter") {
+    click();
+    changeElements();
+  }
+}
+
+resetButton.addEventListener("click", function () {
+  counter = 0;
+  changeElements();
+});
+
+function click() {
+  counter++;
+  if (counter > 100) {
+    counter = 0;
+  }
+}
+
+function changeElements() {
+  document.getElementById("number").innerHTML = counter;
+  root.style.setProperty("--value", counter + "%");
+}
