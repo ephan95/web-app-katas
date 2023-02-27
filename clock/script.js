5a9e8e4c06a26565f1b884f660a72f93d6b27a81
Katharina Ephan <ephankatharina@gmail.com>
2023-02-27 12:47:34 +0100

    made clock code more concise

--- clock/script.js
+++ clock/script.js
@@ -13,15 +13,14 @@ function logTime() {
 
 function changeElements(changedText, hourtime, minutestime, secondstime) {
   document.getElementById("digitalTime").innerText = changedText;
-  root.style.setProperty(
-    "--hourRotation",
-    (hourtime % 12) * 30 + (minutestime / 60) * 30 + 160 + "deg"
-  );
-  root.style.setProperty("--minuteRotation", minutestime * 6 + 160 + "deg");
-  root.style.setProperty("--secondRotation", secondstime * 6 + 160 + "deg");
+  const hourRotation = (hourtime % 12) * 30 + (minutestime / 60) * 30 + 160;
+  const minuteRotation = minutestime * 6 + 160;
+  const secondRotation = secondstime * 6 + 160;
+  root.style.setProperty("--hourRotation", `${hourRotation}deg`);
+  root.style.setProperty("--minuteRotation", `${minuteRotation}deg`);
+  root.style.setProperty("--secondRotation", `${secondRotation}deg`);
 }
 
 logTime();
-changeElements();
 
 setInterval(logTime, 1000);
